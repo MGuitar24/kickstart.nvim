6de568261e654ce46d0205fe3749ccd6c9c89961
MGuitar24 <mguitar24@gmail.com>
2024-11-29 17:58:21 -0700

    enabled neo-tree plugin, created keymap toggle for relative line numbers

--- init.lua
+++ init.lua
@@ -102,7 +102,10 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
+vim.keymap.set('n', '<leader>ln', function()
+  vim.o.relativenumber = not vim.o.relativenumber
+end, { desc = 'Toggle relative line numbers' })
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -931,7 +934,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
